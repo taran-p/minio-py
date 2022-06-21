cbf54b1e2f927f2e88009f47508485bb78f26cc8
taran-p <84724408+taran-p@users.noreply.github.com>
2022-06-21 14:30:17 -0400

    Allow list_buckets to access local server

--- examples/list_buckets.py
+++ examples/list_buckets.py
@@ -14,14 +14,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 from minio import Minio
 
-client = Minio(
-    "play.min.io",
-    access_key="Q3AM3UQ867SPQQA43P2F",
-    secret_key="zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
-)
+def connect()->Minio:
+    url = os.environ.get("MINIO_ADDRESS")
+    user = os.environ.get("MINIO_ACCESS_KEY")
+    pw = os.environ.get("MINIO_SECRET_KEY")
 
-buckets = client.list_buckets()
-for bucket in buckets:
-    print(bucket.name, bucket.creation_date)
+    if url or user or pw:
+        client = Minio(
+            url,
+            access_key=user,
+            secret_key=pw,
+            secure=False
+        )
+    else:
+        client = Minio(
+            'play.min.io',
+            access_key='Q3AM3UQ867SPQQA43P2F',
+            secret_key='zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
+        )
+    return client
+
+
+def main():
+    client = connect()
+
+    buckets = client.list_buckets()
+    for bucket in buckets:
+        print(bucket.name, bucket.creation_date)
